ef30dd8fc66cfaa76e546dbfd3f1ce408d7f987d
Yousef Ali <you@example.com>
2024-10-25 20:33:08 +0300

    Add More Features

---
+++ First-MVC/Controllers/BindingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace First_MVC.Controllers
+{
+    public class BindingController : Controller
+    {
+        //Request HTML (data)
+        //Binidion Primitive (int, string ...
+        public IActionResult TestPrimitive
+            (string Name , int age, int id, string[] color)
+        {
+            return Content($"{Name} : {id} {age}");
+        }
+        public IActionResult TestDic(Dictionary<string,string> Phones)
+        {
+            return View("OK");
+        }
+    }
+}
--- First-MVC/Controllers/EmployeeController.cs
+++ First-MVC/Controllers/EmployeeController.cs
@@ -2,6 +2,7 @@
 using First_MVC.Models.Entities;
 using First_MVC.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 
 namespace First_MVC.Controllers
@@ -9,6 +10,53 @@ namespace First_MVC.Controllers
     public class EmployeeController : Controller
     {
         AppDBContext Context = new AppDBContext();
+
+        public IActionResult Index()
+        {
+            return View("Index",Context.Employee.ToList());
+        }
+        public IActionResult Edit(int id)
+        {
+            if (id != 0)
+            {
+                Employee Emp = Context.Employee
+                    .First(i=>i.Id==id);
+                return View("Edit", Emp);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        public IActionResult SaveEdit(Employee empFromRequest,int id)
+        {
+            if (empFromRequest.Name is not null)
+            {
+                Employee EmpfromDB = Context.Employee
+                    .FirstOrDefault(e => e.Id == id);
+                if (EmpfromDB != null)
+                {
+                    EmpfromDB.Address = empFromRequest.Address;
+                    EmpfromDB.Name = empFromRequest.Name;
+                    EmpfromDB.Salary = empFromRequest.Salary;
+                    EmpfromDB.JobTilte = empFromRequest.JobTilte;
+                    EmpfromDB.ImageURL = empFromRequest.ImageURL;
+                    EmpfromDB.DepartmentID = empFromRequest.DepartmentID;
+                    Context.SaveChanges();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+
+            }
+            return View("Edit",empFromRequest);
+
+        }
+
+
+
+
 
         public IActionResult Details(int id)
         {
@@ -48,5 +96,6 @@ namespace First_MVC.Controllers
 
             return View("DetailsVM", EmpVm);// type==>EmpDeptColorTempMsgBrchViewModel
         }
+
     }
 }
